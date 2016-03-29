bf67f04e969f393628b74372dc3fd7ebbaa84cc3
Marion <24digital@gmail.com>
2016-03-29 18:22:08 -0400

    initial

---
+++ src/main/java/Logic/FileManager.java
@@ -0,0 +1,8 @@
+package Logic;
+
+/**
+ * Created by Marion on 03/29/16.
+ */
+public class FileManager {
+
+}
---
+++ src/main/java/Logic/Processor.java
@@ -0,0 +1,10 @@
+package Logic;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Marion on 03/29/16.
+ */
+@Component
+public class Processor {
+}
