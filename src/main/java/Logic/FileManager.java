bda7bd376c3058460c1e7ecbdabb73d13f2aee0b
Marion <24digital@gmail.com>
2016-04-03 15:58:43 -0400

    Added FileManager class with a thread skeleton

--- src/main/java/Logic/FileManager.java
+++ src/main/java/Logic/FileManager.java
@@ -1,8 +1,28 @@
 package Logic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
 /**
  * Created by Marion on 03/29/16.
  */
+@EnableScheduling
+@EnableAsync
+@Configuration
+@Component
 public class FileManager {
+
+    @Scheduled(fixedRate = 500)
+    public void checkTarger()
+    {
+        System.out.println("Hello ");
+    }
 
 }
