a16a3f2c3b4df29238417ffa16b40a5c3c025729
Marion <24digital@gmail.com>
2016-04-06 19:43:52 -0400

    removed some extra code

--- src/main/java/GUI/Application.java
+++ src/main/java/GUI/Application.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.nio.IntBuffer;
 import java.nio.file.FileSystem;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -37,17 +38,14 @@ public class Application   {
     public static void main(String... args) throws Exception {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        System.out.println("Hello to File Manager");
 
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+
+
+
 
         FileManager manager = ctx.getBean(FileManager.class);
 
-File k = new File("c");
 
         manager.checkTarger(new File("c:\\Users\\Marion\\Downloads\\hwk2.txt").toPath(),new File("C:\\Users\\Marion\\Downloads\\TurtleBeach\\test.txt").toPath());
 
